5eda1fd00b646f08f442f4cf08a7203fa6b359ee
nghiattr <nghiavodich2@gmail.com>
2022-03-08 00:18:13 +0700

    gfdgdf

--- README.md
+++
@@ -1,3 +0,0 @@
-# Flask Docker
-
-This project is a demo of making Docker Image for a python/flask project.
--- terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-    project = "cicd-nghia-test"
-    region = "asia-southeast1-a"
-    credentials = "key.json"
-}
